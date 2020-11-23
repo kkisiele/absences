a83147531a64219f23207e80c3fe151372895197
Karol Kisielewski <kkisiele@gmail.com>
2020-11-23 22:58:05 +0100

    remove unused constructor of AbsenceRejected class

--- src/main/java/com/kkisiele/absence/AbsenceRejected.java
+++ src/main/java/com/kkisiele/absence/AbsenceRejected.java
@@ -3,10 +3,6 @@ package com.kkisiele.absence;
 public class AbsenceRejected extends RuntimeException {
     private final AbsenceRejectionReason reason;
 
-    public AbsenceRejected() {
-        this(null);
-    }
-
     public AbsenceRejected(AbsenceRejectionReason reason) {
         this.reason = reason;
     }
